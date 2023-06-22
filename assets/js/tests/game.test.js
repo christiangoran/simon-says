e1173457a0acf2b22105c2142a189a524130d4e1
christiangoran <christian.goran@protonmail.com>
2023-06-22 14:45:57 +0200

    Mega push

---
+++ assets/js/tests/game.test.js
@@ -0,0 +1,119 @@
+/**
+ * @jest-environment jsdom
+ */ 
+
+
+const {game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn} = require('../game');
+
+jest.spyOn(window, 'alert').mockImplementation(() => { });
+
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html', 'utf-8');
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe('game object contains correct keys', () => {
+    test('score key exists', () => {
+        expect('score' in game).toBe(true);
+    });
+    test('current game array exist', () => {
+        expect('currentGame' in game).toBe(true);
+    });
+    test('playerMoves array exists', () => {
+        expect('playerMoves' in game).toBe(true);
+    });
+    test('choices array exist', () => {
+        expect('choices' in game).toBe(true);
+    });
+    test('turnNumber variable exist', () => {
+        expect('turnNumber' in game).toBe(true);
+    });
+    test('lastButton exists', () => {
+        expect('lastButton' in game).toEqual(true);
+    });
+    test('turnInProgress exists', () => {
+        expect('turnInProgress' in game).toEqual(true);
+    });
+    test('choices contains correct ids', () => {
+        expect(game.choices).toEqual(['button1', 'button2', 'button3', 'button4'])
+    });
+});
+
+describe('newGame works correctly', () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ['button1','button2','button3'];
+        game.currentGame = ['button1','button2','button3'];
+        document.getElementById('score').innerText = "42";
+        newGame();
+    });
+    test('Should set the game score to zero', () => {
+        expect(game.score).toEqual(0);
+    });
+    test('Should display 0 for the element with id of score', () => {
+        expect(document.getElementById('score').innerText).toEqual(0);
+    });
+    test('Should clear the playerMoves array', () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+   test('currentGame should contain 1 item', () => {
+    expect(game.currentGame.length).toBe(1)
+   });
+   test('expect data-listener to be true', () => {
+    const elements = document.getElementsByClassName('circle');
+    for(let element of elements) {
+        expect(element.getAttribute('data-listener')).toEqual('true');
+    }
+   });
+});
+
+describe('Gameplay works correctly', () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test('addTurn adds a new turn to the game', () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    })
+    test('Should add correct class to light up the buttons', () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain('light');
+    });
+    test('showTurns should update game.turnNumber', () => {
+        game.turnNumber = 42;
+        showTurns()
+        expect(game.turnNumber).toBe(0);
+    });
+    test('Should increment the score if the turn is correct', () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    });
+    test('Should call an alert if the move is wrong', () => {
+        game.playerMoves.push('wrong');
+        playerTurn();
+        expect(window.alert).toBeCalledWith('Wrong move!');
+    });
+    test('Should check that turnInProgress is true', () => {
+        showTurns();
+        expect(game.turnInProgress).toBe(true)
+    });
+    test('clicking during computer sequence should fail', () => {
+        showTurns();
+        game.lastButton = '';
+        document.getElementById('button2').click();
+        expect(game.lastButton).toEqual('');
+    })
+});
